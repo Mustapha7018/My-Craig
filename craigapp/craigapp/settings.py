67c8ef7d7392c84a78da98ae60641a044b77699d
Mustapha Adams <adamsmustapha419@gmail.com>
2022-08-05 01:44:06 +0000

    another commit

--- craigapp/craigapp/settings.py
+++ craigapp/craigapp/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-# from pathlib import Path
-# from re import TEMPLATE
+from pathlib import Path
+from re import TEMPLATE
 
 import os
 
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "craigapp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
