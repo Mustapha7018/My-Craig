5cfa301d9cdc8279704aa5d55fb66dc5dbaab009
Mustapha Adams <adamsmustapha419@gmail.com>
2022-08-11 01:08:43 +0000

    search functionality

--- craigapp/myapp/models.py
+++ craigapp/myapp/models.py
@@ -6,7 +6,11 @@ class Search(models.Model):
     search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
 
+    # def __str__(self):
+    #     return '{}'.format(self.search)
+
     def __str__(self):
-        return '{}'.format(self.search)
+        return f'{self.search}'
+    
     class Meta:
         verbose_name_plural = 'Searches'
